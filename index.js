444b1f4d89d7786cb58325943cf47c3b81018c8a
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-03-29 19:23:13 +0600

    Backend Started Pack Created

--- index.js
+++ index.js
@@ -1,7 +1,38 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri = `mongodb+srv://${process.env.HOUSENEST_USERNAME}:${process.env.HOUSENEST_PASSWORD}@cluster0.2xcsswz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+console.log(uri);
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
   res.send("House-Property Running on 5000 ---->");
@@ -10,3 +41,6 @@ app.get("/", (req, res) => {
 app.listen(port, () => {
   console.log(`House-Property Currently Running on :----> ${port}`);
 });
+
+// CDz3DDuUjZwTa3Vq
+// housenestDB
