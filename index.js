ddecf9191f29765c4137b20a7814f83361a2c864
mdimranhossainwd <mdimranhossain7498@gmail.com>
2024-04-01 15:33:58 +0600

    Whislist and Review Post method Compleed

--- index.js
+++ index.js
@@ -4,7 +4,15 @@ require("dotenv").config();
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.HOUSENEST_USERNAME}:${process.env.HOUSENEST_PASSWORD}@cluster0.2xcsswz.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 console.log(uri);
@@ -24,10 +32,41 @@ async function run() {
       .db("housenestDB")
       .collection("advertisement");
 
+    const reviewCollections = client.db("housenestDB").collection("reviews");
+    const wishlistsCollections = client
+      .db("housenestDB")
+      .collection("wishlists");
+
     // CURD OPERATIONS
-    app.get("/housenest/v1/advertisement", async (req, res) => {
+    app.get("/housenest/api/v1/advertisement", async (req, res) => {
       const cursor = await advertisementCollections.find().toArray();
       res.send(cursor);
+    });
+
+    app.get("/housenest/v1/advertisement/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await advertisementCollections.findOne(query);
+      res.send(result);
+    });
+
+    app.post("/housenest/api/v1/reviews", async (req, res) => {
+      const body = req.body;
+      const result = await reviewCollections.insertOne(body);
+      res.send(result);
+    });
+
+    app.post("/housenest/api/v1/wishlists", async (req, res) => {
+      const body = req.body;
+      const result = await wishlistsCollections.insertOne(body);
+      res.send(result);
+    });
+
+    app.get("/housenest/api/v1/wishlists/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await wishlistsCollections.findOne(query);
+      res.send(result);
     });
 
     // Connect the client to the server	(optional starting in v4.7)
